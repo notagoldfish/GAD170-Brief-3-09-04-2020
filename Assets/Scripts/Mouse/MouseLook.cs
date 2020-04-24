869f51a9356fef8f1b95e2e9af252d5f502204f0
rdreilly217 <61007868+rdreilly217@users.noreply.github.com>
2020-04-24 21:39:29 +0800

    charactermovement, sprint double jump, mouselook

--- Assets/Scripts/Mouse/MouseLook.cs
+++ Assets/Scripts/Mouse/MouseLook.cs
@@ -2,48 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
+public class mouseLook : MonoBehaviour
 {
-    //Mouse Camera Rotation X axis
     public float rotX;
-    //Mouse Camera Rotation Y axis
     public float rotY;
-    //Mouse Camera Rotation Z axis
     public float rotZ;
-    // Player Rotation Speed
     public float rotationSpeed;
-
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+        rotX -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed; //speed of mouse camera
+        rotY += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed; //speed of mouse camera
+
+        if (rotX < -90)
         {
-            //Rotation speed / Mouse Sensitivity
-            rotX -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
-            rotY += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
-
-            if (rotX < -90)
-            {
-                rotX = -90;
-            }
-            else if (rotX > 90)
-            {
-                rotX = 90;
-            }
-
-            //transform.rotation = Quaternion.Euler(0, rotY, 0);
-            this.GetComponent<Rigidbody>().rotation = Quaternion.Euler(0, rotY, 0);
-            GameObject.FindWithTag("MainCamera").transform.rotation = Quaternion.Euler(rotX, rotY, 0);
-            //this.GetComponent<Rigidbody>().velocity += Vector3.up * this.jumpHeight;
-
+            rotX = -90;
         }
+        else if (rotX > 90) //will stop constant rotation of camera when looking down
+        {
+            rotX = 90;
+        }
+
+        transform.rotation = Quaternion.Euler(0, rotY, 0);
+        GameObject.FindWithTag("MainCamera").transform.rotation = Quaternion.Euler(rotX, rotY, 0);
     }
 }
-
-
-
---
+++ Assets/Scripts/PlayerMovement/character.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float rotateSpeed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private int jumps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   // Left shift sprint will only work when w key (forward is pressed)
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed * 1.5f;
+        }
+        else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift))
+
+
+        {
+            if (controller.isGrounded)
+            {
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    moveDirection.y = jumpSpeed;
+                }
+                jumps = 0;
+            }
+            else
+            {
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection.x *= speed;
+                moveDirection.z *= speed;
+                if (Input.GetKeyDown(KeyCode.Space) && jumps < 1)
+                {
+                    moveDirection.y = jumpSpeed;
+                    jumps++;
+                }
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+    }
+}
