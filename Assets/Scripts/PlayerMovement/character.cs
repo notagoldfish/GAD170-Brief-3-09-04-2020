407d3541798364fb8028e46058f1dde1ae74c083
rdreilly217 <61007868+rdreilly217@users.noreply.github.com>
2020-04-26 16:45:30 +0800

    removed sprint character controller works now

--- Assets/Scripts/PlayerMovement/character.cs
+++ Assets/Scripts/PlayerMovement/character.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class character : MonoBehaviour
 {
-    public float speed = 6.0f;
+    public float speed = 12.0f;
     public float rotateSpeed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
@@ -21,41 +21,33 @@ public class character : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   // Left shift sprint will only work when w key (forward is pressed)
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w"))
+    {
+        if (controller.isGrounded)
         {
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed * 1.5f;
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            jumps = 0;
         }
-        else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift))
-
-
+        else
         {
-            if (controller.isGrounded)
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= speed;
+            moveDirection.z *= speed;
+            if (Input.GetKeyDown(KeyCode.Space) && jumps < 1)
             {
-                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-                moveDirection = transform.TransformDirection(moveDirection);
-                moveDirection *= speed;
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    moveDirection.y = jumpSpeed;
-                }
-                jumps = 0;
+                moveDirection.y = jumpSpeed;
+                jumps++;
             }
-            else
-            {
-                moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
-                moveDirection = transform.TransformDirection(moveDirection);
-                moveDirection.x *= speed;
-                moveDirection.z *= speed;
-                if (Input.GetKeyDown(KeyCode.Space) && jumps < 1)
-                {
-                    moveDirection.y = jumpSpeed;
-                    jumps++;
-                }
-            }
-
-            moveDirection.y -= gravity * Time.deltaTime;
-            controller.Move(moveDirection * Time.deltaTime);
         }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
+
