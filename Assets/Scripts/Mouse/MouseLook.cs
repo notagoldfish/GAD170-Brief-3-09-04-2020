5213dbbd90768936976d556732ea77a5d58bf496
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-04-27 19:37:45 +0800

    Minor Changes
    Not major changes

--- Assets/Scripts/Mouse/MouseLook.cs
+++ Assets/Scripts/Mouse/MouseLook.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mouseLook : MonoBehaviour
+public class MouseLook : MonoBehaviour
 {
     public float rotX;
     public float rotY;
