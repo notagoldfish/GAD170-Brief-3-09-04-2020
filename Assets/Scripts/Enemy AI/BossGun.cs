8ddd20035e4a2496a29a0214c87be474c841d845
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-04-29 01:55:24 +0800

    Added Boss and Floor
    Added in boss and a bottom to the level, will add a collider that kills that player if they fall once the restart is completed

---
+++ Assets/Scripts/Enemy AI/BossGun.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGun : MonoBehaviour
+{
+
+    public float damage;
+    public float range;
+    public KeyCode interactKey; //Allows for changing the interact button in the editor
+    public float fireRate;
+    public ParticleSystem muzzleFlashOne;
+    public ParticleSystem muzzleFlashTwo;
+    public ParticleSystem muzzleFlashThree;
+    public ParticleSystem muzzleFlashFour;
+    public ParticleSystem muzzleFlashFive;
+    public GameObject impactEffect;
+    Transform target;
+    public float viewCone;
+    private float shootAmount = 5;
+
+    private float nextTimeToFire = 0f;
+
+    private void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+
+    }
+
+    void Update()
+    {
+        //Line of Sight
+        float dot = Vector3.Dot(transform.forward, (target.position - transform.position).normalized);
+        if (dot > viewCone)
+        {
+            if (Time.time >= nextTimeToFire && Vector3.Distance(target.transform.position, this.transform.position) <= range)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                while(shootAmount > 0)
+                {
+                    Shoot();
+                    shootAmount--;
+                    Debug.Log("I Shot!");
+                }
+                shootAmount = 5;
+            }
+            //Debug.Log("Quite facing");
+        }
+
+    }
+
+    void Shoot()
+    {
+        muzzleFlashOne.Play();
+        muzzleFlashTwo.Play();
+        muzzleFlashThree.Play();
+        muzzleFlashFour.Play();
+        muzzleFlashFive.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, range))
+        {
+            //Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+
+        }
+    }
+}
+
