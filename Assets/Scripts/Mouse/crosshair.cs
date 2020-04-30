18514eb28f9830b179c1d649898fbe54ffa0fc7c
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-04-30 18:44:06 +0800

    Crosshair Adjustment
    Fiddled with crosshair to get it to change size mid-game

--- Assets/Scripts/Mouse/crosshair.cs
+++ Assets/Scripts/Mouse/crosshair.cs
@@ -9,15 +9,20 @@ public class crosshair : MonoBehaviour
 
     void Start()
     {
-        float crosshairSize = Screen.width * 0.1f;
-        crosshairTexture = Resources.Load ("Textures/crosshair") as Texture;
-        crosshairRect = new Rect(Screen.width / 2 - crosshairSize / 2,
-            Screen.height / 2 - crosshairSize / 2,
-            crosshairSize, crosshairSize);
+
     }
 
     void OnGUI()
     {
         GUI.DrawTexture(crosshairRect, crosshairTexture);
+    }
+
+    private void Update()
+    {
+        float crosshairSize = Screen.width * 0.05f;
+        crosshairTexture = Resources.Load("Textures/crosshair") as Texture;
+        crosshairRect = new Rect(Screen.width / 2 - crosshairSize / 2,
+        Screen.height / 2 - crosshairSize / 2,
+        crosshairSize, crosshairSize);
     }
 }
