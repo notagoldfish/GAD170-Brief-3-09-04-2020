136188a492f3c5735e35190d88acb04c2d3783ce
rdreilly217 <61007868+rdreilly217@users.noreply.github.com>
2020-04-30 18:48:55 +0800

    update projectile/light

---
+++ Assets/Scripts/Interacting/bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Mouse/ProjectileShooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShooting : MonoBehaviour
+{
+    //create projectile
+    //place projectile into resources folder
+    //add rigidbody to projectile/prefab
+
+    public GameObject prefab;
+
+
+
+    void Start()
+    {
+        prefab = Resources.Load("projectile") as GameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward * 50;
+            Destroy(projectile, 05f);
+
+
+        }
+
+
+
+    }
+}
