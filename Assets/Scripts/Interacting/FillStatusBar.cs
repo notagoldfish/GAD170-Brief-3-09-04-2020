b92d7d234c8ddd16bbde16dec652848e24b6711d
rdreilly217 <61007868+rdreilly217@users.noreply.github.com>
2020-05-01 16:21:32 +0800

    slider health

---
+++ Assets/Scripts/Interacting/FillStatusBar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FillStatusBar : MonoBehaviour
+
+{
+
+    public character playerHealth;
+    public Image fillImage;
+    private Slider slider;
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+
+        if (slider.value > slider.minValue && !fillImage.enabled)
+        {
+            fillImage.enabled = true;
+        }
+        float fillValue = playerHealth.currentHealth / playerHealth.maxHealth;
+        if(fillValue <= slider.maxValue / 3)
+        {
+            fillImage.color = Color.red;
+        }
+
+
+
+       //
+       //else if (fillValue > slider.maxValue / 3)
+       //{
+       //   fillImage.color = Color.green;
+       //}
+       // slider.value = fillValue;
+    }
+
+}
